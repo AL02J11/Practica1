99233eb9929fd546cbf4838c64aa64b8d1b18adc
Katia <katiabc09@gmail.com>
2013-03-20 08:00:23 -0600

    Version1.0

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+$(document).ready(function(e) {
+	//alert ("listo")
+	document.addEventListener("deviceready",function (){
+		$('.btn').click (function(){
+		var boton=(($(this).attr('class')).split(' '))[1];
+		switch(boton)
+		{
+			case 'b1':
+			navigator.notification.beep(1);
+			break;
+			case 'b2':
+			navigator.notification.beep(2);
+			break;
+			case 'b3':
+			navigator.notification.beep(3);
+			break;
+			case 'v1':
+			navigator.notification.vibrate(500);
+			break;
+			case 'v2':
+			navigator.notification.vibrate(300);
+			navigator.notification.vibrate(1000);
+			break;
+		}
+		});
+		$('#derecha').swiperight(function(){
+			navigator.notification.alert("Deslizo a la derecha",function(){ },"Practica 1","Aceptar");
+		});
+	},false);
+    });
